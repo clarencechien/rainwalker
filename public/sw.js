4ca02338950a6a6a3159ac6a63b2536b38d6387b
clarencechien <7018587+clarencechien@users.noreply.github.com>
2026-06-26 13:44:26 +0800

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rain-v8";
+const CACHE = "rain-v9";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
